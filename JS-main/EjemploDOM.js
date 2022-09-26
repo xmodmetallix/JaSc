87ac9b45f3a36a3a469cf0fd1afa43890c46bc28
Arturo Amador <arturo.amadorb@hotmail.com>
2022-09-25 22:55:45 -0500

    Storage

--- JS-main/EjemploDOM.js
+++ JS-main/EjemploDOM.js
@@ -1,35 +1,36 @@
 const inputNombre = document.getElementById("nombre");
 const inputApellido = document.getElementById("apellido");
-const inputMail = document.getElementById("mail");
+const inputMail = document.getElementById("email");
 const inputPassword = document.getElementById("password");
 const botonIngresar = document.getElementById("botonIngresar");
 const divDatos = document.getElementById("DivDatos");
+const DivBienvenida = document.getElementById("DivBienvenida");
 
 let productos = [];
 
 const selectTag = document.getElementById('lista');
 
 const Guitarra = {
-    Marca: "Gibson",
-    Precio: 25000,
-    Modelo: "Les Paul",
-    Descripcion: "Madera de cedro"
+  Marca: "Gibson",
+  Precio: 25000,
+  Modelo: "Les Paul",
+  Descripcion: "Madera de cedro"
 }
 productos.push(Guitarra);
 
 const Bajo = {
-    Marca: "Fender",
-    Precio: 15500,
-    Modelo: "Squier",
-    Descripcion: "Madera de pino"
+  Marca: "Fender",
+  Precio: 15500,
+  Modelo: "Squier",
+  Descripcion: "Madera de pino"
 }
 productos.push(Bajo);
 
 const Bateria = {
-    Marca: "Tama",
-    Precio: 35000,
-    Modelo: "Club Jam",
-    Descripcion: "Color rojo"
+  Marca: "Tama",
+  Precio: 35000,
+  Modelo: "Club Jam",
+  Descripcion: "Color rojo"
 }
 productos.push(Bateria);
 
@@ -40,28 +41,16 @@ botonIngresar.onclick = () => {
   const usuario = {
     nombre: inputNombre.value,
     apellido: inputApellido.value,
-    mail: inputMail.value,
-    password: inputPassword.value
+    email: inputMail.value,
+    password: inputPassword.value,
   }
-
   localStorage.setItem('usuarioStorage', JSON.stringify(usuario))
-  divDatos.remove();
-  crearSaludo(usuario);
-
-  productos.forEach((producto) => {
-    const option = document.createElement('option');
-    option.innerText = `${producto.Marca}, ${producto.Modelo}: $${producto.Precio}`;
-    selectTag.append(option);
-  })
 }
-
-function crearSaludo(user) {
-  const saludarTitulo = document.createElement('h2')
-  saludarTitulo.innerText = `Bienvenido a Bonehead ${user.nombre} ${user.apellido}`
-  divSaludo.append(saludarTitulo)
-}
-
-
+productos.forEach((producto) => {
+  const option = document.createElement('option');
+  option.innerText = `${producto.Marca}, ${producto.Modelo}: $${producto.Precio}`;
+  selectTag.append(option);
+})
 
 //EVENTOS
 
@@ -91,4 +80,3 @@ boton2.onclick = () => {
   p.innerText = `El Precio total a pagar es de : $${totalCompra}`;
   document.body.append(p);
 }
-
